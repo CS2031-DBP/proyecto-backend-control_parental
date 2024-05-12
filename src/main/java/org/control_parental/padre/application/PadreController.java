d661ae215d3de67b703ccbd294dda8b2cd6fb6d0
m41k12 <michael.hinojosa@utec.edu.pe>
2024-05-12 09:47:58 -0500

    no cree ninguna funcion nueva, cambie las dos existentes para que sean con dtos.

--- src/main/java/org/control_parental/padre/application/PadreController.java
+++ src/main/java/org/control_parental/padre/application/PadreController.java
@@ -3,6 +3,8 @@ package org.control_parental.padre.application;
 import jakarta.validation.Valid;
 import org.control_parental.padre.domain.Padre;
 import org.control_parental.padre.domain.PadreService;
+import org.control_parental.padre.dto.NewPadreDto;
+import org.control_parental.padre.dto.PadreResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +17,14 @@ public class PadreController {
     PadreService padreService;
 
     @PostMapping
-    ResponseEntity<Void> savePadre(@Valid @RequestBody Padre padre) {
-        padreService.savePadre(padre);
+    ResponseEntity<Void> savePadre(@Valid @RequestBody NewPadreDto newPadreDto) {
+        padreService.savePadre(newPadreDto);
         return ResponseEntity.created(null).build();
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<Padre> getPadreById(@PathVariable Long id) {
-        Padre padre = padreService.getPadreById(id);
-        return ResponseEntity.ok(padre);
+    ResponseEntity<PadreResponseDto> getPadreById(@PathVariable Long id) {
+        PadreResponseDto padreResponseDto = padreService.getPadreById(id);
+        return ResponseEntity.ok(padreResponseDto);
     }
 }
--- src/main/java/org/control_parental/padre/domain/PadreService.java
+++ src/main/java/org/control_parental/padre/domain/PadreService.java
@@ -1,19 +1,33 @@
 package org.control_parental.padre.domain;
 
+import org.control_parental.padre.dto.NewPadreDto;
+import org.control_parental.padre.dto.PadreResponseDto;
 import org.control_parental.padre.infrastructure.PadreRepository;
+import org.control_parental.usuario.domain.Role;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PadreService {
     @Autowired
-    PadreRepository repository;
+    PadreRepository padreRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public void savePadre(Padre padre) {
-        repository.save(padre);
+    public void savePadre(NewPadreDto newPadreDto) {
+        Padre padre = modelMapper.map(newPadreDto, Padre.class);
+        padre.setRole(Role.PADRE);
+        padreRepository.save(padre);
+
     }
 
-    public Padre getPadreById(Long id) {
-        return repository.findById(id).orElseThrow();
+    public PadreResponseDto getPadreById(Long id) {
+        Padre padre  = padreRepository.findById(id).orElseThrow();
+
+        return modelMapper.map(padre, PadreResponseDto.class);
     }
+
+
+
 }
