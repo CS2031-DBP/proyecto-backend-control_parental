bc78333feae0e81b12b65d45b08cf66450e6767c
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2024-05-12 20:36:14 -0500

    LINEA 44, permitAll debe ser cambiado a authenticated(), por ahora podemos seguir testeando sin problemas

--- src/main/java/org/control_parental/configuration/SecurityConfig.java
+++ src/main/java/org/control_parental/configuration/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/auth/**").permitAll()
-                                .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
                 .build();
     }
