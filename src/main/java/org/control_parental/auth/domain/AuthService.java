a56ca239da955870b4b55a5c6095086b9fdd68ed
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2024-05-19 13:50:24 -0500

    expeption to exception

---
+++ src/main/java/org/control_parental/auth/domain/AuthService.java
@@ -0,0 +1,47 @@
+package org.control_parental.auth.domain;
+
+import org.control_parental.admin.infrastructure.AdminRepository;
+import org.control_parental.auth.dto.AuthJwtResponse;
+import org.control_parental.auth.dto.AuthLoginRequest;
+import org.control_parental.configuration.JwtService;
+import org.control_parental.exceptions.IllegalArgumentException;
+import org.control_parental.exceptions.ResourceNotFoundException;
+import org.control_parental.padre.domain.Padre;
+import org.control_parental.padre.infrastructure.PadreRepository;
+import org.control_parental.profesor.infrastructure.ProfesorRepository;
+import org.control_parental.usuario.domain.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.control_parental.usuario.infrastructure.UsuarioRepository;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UsuarioRepository<Usuario> usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtService jwtService;
+
+    public AuthJwtResponse login(AuthLoginRequest authLoginRequest) {
+        Usuario usuario = usuarioRepository.findByEmail(authLoginRequest.getEmail()).orElseThrow(() -> new ResourceNotFoundException("username incorrecto"));
+        AuthJwtResponse authJwtResponse = new AuthJwtResponse();
+
+        if(!passwordEncoder.matches(authLoginRequest.getPassword(), usuario.getPassword())) {
+            throw new IllegalArgumentException("Incorrect Password");
+        }
+
+        authJwtResponse.setToken(jwtService.generateToken(usuario));
+        return authJwtResponse;
+    }
+
+
+}
