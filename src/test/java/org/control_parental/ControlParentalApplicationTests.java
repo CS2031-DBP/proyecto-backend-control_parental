1597f3243e30640404d99493f80ec9e54659b191
m41k12 <michael.hinojosa@utec.edu.pe>
2024-07-07 22:06:20 -0500

    tests comentados

--- src/test/java/org/control_parental/ControlParentalApplicationTests.java
+++ src/test/java/org/control_parental/ControlParentalApplicationTests.java
@@ -1,10 +1,10 @@
-package org.control_parental;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ControlParentalApplicationTests {
-	// Elimine void contextLoads() --Michael
-
-}
+//package org.control_parental;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ControlParentalApplicationTests {
+//	// Elimine void contextLoads() --Michael
+//
+//}
