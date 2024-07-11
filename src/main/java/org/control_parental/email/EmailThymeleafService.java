9288ce229283c5c24f6602047f1613a4efe45365
m41k12 <michael.hinojosa@utec.edu.pe>
2024-07-11 00:27:23 -0500

    mail

---
+++ src/main/java/org/control_parental/email/EmailThymeleafService.java
@@ -0,0 +1,45 @@
+package org.control_parental.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.context.Context;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class EmailThymeleafService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
+
+    public void sendEmail(Mail mail) throws MessagingException,IOException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        String html = getHtmlContent(mail);
+
+        helper.setTo(mail.getTo());
+        helper.setFrom(mail.getFrom());
+        helper.setSubject(mail.getSubject());
+        helper.setText(html, true);
+
+        javaMailSender.send(message);
+    }
+
+    private String getHtmlContent(Mail mail) {
+        Context context = new Context();
+        context.setVariables(mail.getHtmlTemplate().getProps());
+        return springTemplateEngine.process(mail.getHtmlTemplate().getTemplate(), context);
+    }
+
+}
